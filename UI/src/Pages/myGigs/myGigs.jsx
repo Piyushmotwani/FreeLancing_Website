09187b21d5bdfc24898bff2e26bfeec321eb7ca8
Piyush Motwani <piyushmotwani211@gmail.com>
2023-03-19 12:16:16 +0530

    Initial-set-up-done

---
+++ UI/src/Pages/myGigs/myGigs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './myGigs.scss'
+const myGigs = () => {
+  return (
+    <div className='myGigs'>myGigs</div>
+  )
+}
+
+export default myGigs
---
+++ UI/src/Pages/myOrders/myOrders.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './myOrders.scss'
+const myOrders = () => {
+  return (
+    <div className='my-orders'>myOrders</div>
+  )
+}
+
+export default myOrders
